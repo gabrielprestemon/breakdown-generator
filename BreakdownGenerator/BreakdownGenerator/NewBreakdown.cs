463f34af4d53b6efc1122dbaaabb29ab05b8c96f
Gabriel <gabriel.prestemon@gmail.com>
2019-04-25 21:22:23 -0700

    Change project to netcore and add PS dependency

---
+++ BreakdownGenerator/BreakdownGenerator/NewBreakdown.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace BreakdownGenerator
+{
+    [Cmdlet(VerbsCommon.New, "Breakdown")]
+    class NewBreakdown : PSCmdlet
+    {
+    }
+}
